476df52f3f2fb85210e25868d976b9db0397c41d
Vinicius Faria <viniciusfaria12@outlook.com>
2025-03-26 14:01:59 -0300

    att

--- JavaAlura/src/main/java/com/example/JavaAlura/FilmesApplication.java
+++ JavaAlura/src/main/java/com/example/JavaAlura/FilmesApplication.java
@@ -1,16 +1,9 @@
 package com.example.JavaAlura;
 
-import com.example.JavaAlura.Model.DadosEpisodio;
-import com.example.JavaAlura.Model.DadosSerie;
-import com.example.JavaAlura.Model.DadosTemporada;
-import com.example.JavaAlura.Service.ConsumoApi;
-import com.example.JavaAlura.Service.ConverteDados;
+import com.example.JavaAlura.principal.Principal;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
-import java.util.ArrayList;
-import java.util.List;
 
 @SpringBootApplication
 public class FilmesApplication implements CommandLineRunner {
@@ -21,24 +14,8 @@ public class FilmesApplication implements CommandLineRunner {
 
 	@Override
 	public void run(String... args) {
-		var consumoApi = new ConsumoApi();
-		var json = consumoApi.obterDados("https://www.omdbapi.com/?t=The+Flash&apikey=18bacbdb");
-		System.out.println(json);
-        ConverteDados conversor = new ConverteDados();
-		DadosSerie dados = conversor.obterDados(json, DadosSerie.class);
-		System.out.println(dados);
-		json = consumoApi.obterDados("https://www.omdbapi.com/?t=The+Flash&Season=1&episode=1&apikey=18bacbdb");
-		DadosEpisodio dadosEpisodio = conversor.obterDados(json, DadosEpisodio.class);
-		System.out.println(dadosEpisodio);
+        Principal principal = new Principal();
+        principal.exibMenu();
 
-		List<DadosTemporada> temporadas = new ArrayList<>();
-
-		for(int i = 1; i<=dados.totalTemporadas(); i++){
-			json = consumoApi.obterDados("https://www.omdbapi.com/?t=The+Flash&season=" + i + "&apikey=18bacbdb");
-			DadosTemporada dadosTemporada = conversor.obterDados(json, DadosTemporada.class);
-			temporadas.add(dadosTemporada);
-
-		}
-		temporadas.forEach(System.out::println);
 	}
 }
