5a0c39fa4995b4d69d80fb347a68bfacdf304899
Vinicius Faria <viniciusfaria12@outlook.com>
2025-04-01 10:46:31 -0300

    erro gpt

--- JavaAlura/src/main/java/com/example/JavaAlura/Service/consultaChatGPT.java
+++ JavaAlura/src/main/java/com/example/JavaAlura/Service/consultaChatGPT.java
@@ -4,7 +4,7 @@ import com.theokanning.openai.completion.CompletionRequest;
 import com.theokanning.openai.service.OpenAiService;
 public class consultaChatGPT {
     public static String obterTraducao(String texto) {
-        OpenAiService service = new OpenAiService("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");
+        OpenAiService service = new OpenAiService("inseri o codigo ai meu ");
         CompletionRequest requisicao = CompletionRequest.builder()
                 .model("gpt-3.5-turbo-instruct")
                 .prompt("traduza para o português o texto: " + texto)
