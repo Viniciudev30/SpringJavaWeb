3c95447e8dde91ba985d4126d08e3ac6c6e47c3a
Vinicius Faria <viniciusfaria12@outlook.com>
2025-04-08 15:46:48 -0300

    Banco de dados + java aula categorai

--- JavaAlura/src/main/java/com/example/JavaAlura/principal/Principal.java
+++ JavaAlura/src/main/java/com/example/JavaAlura/principal/Principal.java
@@ -4,7 +4,7 @@ import com.example.JavaAlura.Model.*;
 import com.example.JavaAlura.Service.ConsumoApi;
 import com.example.JavaAlura.Service.ConverteDados;
 import com.example.JavaAlura.repository.SerieRepository;
-import org.springframework.beans.factory.annotation.Autowired;
+
 
 import java.util.*;
 import java.util.stream.Collectors;
@@ -34,7 +34,10 @@ public class Principal {
                     1 - Buscar séries
                     2 - Buscar episódios
                     3 - Listar series buscados
-                                    
+                    4-  Buscar serie por Titulo
+                    5-  Buscar serie por ator       
+                    6-  Top 5 Series
+                    7-  Buscar series por categoria         
                     0 - Sair                                 
                     """;
 
@@ -52,6 +55,18 @@ public class Principal {
                 case 3:
                     listarSeriesbuscadas();
                     break;
+                case 4:
+                    buscarSeriePorTitulo();
+                    break;
+                case 5:
+                    buscarSeriePorAtor();
+                    break;
+                case 6:
+                    buscarTop5Series();
+                    break;
+                case 7:
+                    buscarSeriePorCategoria();
+                    break;
                 case 0:
                     System.out.println("Saindo...");
                     break;
@@ -60,6 +75,8 @@ public class Principal {
             }
         }
     }
+
+
 
     private void buscarSerieWeb() {
         DadosSerie dados = getDadosSerie();
@@ -82,9 +99,7 @@ public class Principal {
         System.out.println("Escolha uma serie para buscar seus devidos episodios");
         var nomeSerie = leitura.nextLine();
 
-        Optional<Serie> serie = series.stream()
-                .filter(s -> s.getTitulo().toLowerCase().contains(nomeSerie.toLowerCase()))
-                .findFirst();
+        Optional<Serie> serie = repositorio.findByTituloContainingIgnoreCase(nomeSerie);
 
         if(serie.isPresent()) {
 
@@ -114,6 +129,42 @@ public class Principal {
         series.stream()
                 .sorted(Comparator.comparing(Serie::getGenero))
                 .forEach(System.out::println);
+    }
+    private void buscarSeriePorTitulo() {
+        System.out.println("Escolha uma serie para buscar seus devidos episodios");
+        var nomeSerie = leitura.nextLine();
+        Optional<Serie> serieBuscada = repositorio.findByTituloContainingIgnoreCase(nomeSerie);
+
+        if (serieBuscada.isPresent()){
+            System.out.println("Dados da serie" + serieBuscada.get());
+        } else {
+            System.out.println("Serie não encontrada:");
+        }
+
+    }
+
+    private void buscarSeriePorAtor() {
+        System.out.println("Qual o nome para buscar?");
+        var nomeAtor = leitura.nextLine();
+        System.out.println("Avaliações a partir de que valor?");
+        var avaliacao = leitura.nextDouble();
+        List<Serie> seriesEncontradas = repositorio.findByAtoresContainingIgnoreCaseAndAvaliacaoGreaterThanEqual(nomeAtor, avaliacao);
+        System.out.println("Series em que " + nomeAtor + "trabalhou: ");
+        seriesEncontradas.forEach(s ->
+                System.out.println(s.getTitulo() + " avaliação:" + s.getAvaliacao()));
+    }
+
+    private void buscarTop5Series(){
+        List<Serie> serieTop = repositorio.findTop5ByOrderByAvaliacaoDesc();
+        serieTop.forEach(s ->
+                System.out.println(s.getTitulo() + " avaliação:" + s.getAvaliacao()));
+    }
+
+    private void buscarSeriePorCategoria(){
+        System.out.println("Deseja buscar series de que categoria/genero");
+        var nomeGenero = leitura.nextLine();
+
+        List<Serie> seriesPorCategoria = repositorio.findByGenero(categoria);
     }
 
 }
--- JavaAlura/src/main/java/com/example/JavaAlura/repository/SerieRepository.java
+++ JavaAlura/src/main/java/com/example/JavaAlura/repository/SerieRepository.java
@@ -1,7 +1,18 @@
 package com.example.JavaAlura.repository;
 
+import com.example.JavaAlura.Model.Categoria;
 import com.example.JavaAlura.Model.Serie;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+import java.util.Optional;
+
 public interface SerieRepository extends JpaRepository<Serie, Long> {
+    Optional<Serie>findByTituloContainingIgnoreCase(String nomeSerie);
+
+    List<Serie> findByAtoresContainingIgnoreCaseAndAvaliacaoGreaterThanEqual(String nomeAtor, Double avaliacao);
+
+    List<Serie> findTop5ByOrderByAvaliacaoDesc();
+
+    List<Serie> findByGenero(Categoria categoria);
 }
