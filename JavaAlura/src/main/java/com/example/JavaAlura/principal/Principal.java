58472dfa54568a4be756dfe1dff562c5b588a66a
Vinicius Faria <viniciusfaria12@outlook.com>
2025-04-10 08:42:00 -0300

    Complete

--- JavaAlura/src/main/java/com/example/JavaAlura/principal/Principal.java
+++ JavaAlura/src/main/java/com/example/JavaAlura/principal/Principal.java
@@ -37,7 +37,8 @@ public class Principal {
                     4-  Buscar serie por Titulo
                     5-  Buscar serie por ator       
                     6-  Top 5 Series
-                    7-  Buscar series por categoria         
+                    7-  Buscar series por categoria
+                    8-  Filtrar séries         
                     0 - Sair                                 
                     """;
 
@@ -66,6 +67,9 @@ public class Principal {
                     break;
                 case 7:
                     buscarSeriePorCategoria();
+                    break;
+                case 8:
+                    filtrarSeriesPorTemporadaEAvaliacao();
                     break;
                 case 0:
                     System.out.println("Saindo...");
@@ -167,6 +171,19 @@ public class Principal {
         List<Serie> seriesPorCategoria = repositorio.findByGenero(categoria);
         System.out.println("Series da categoria" + nomeGenero);
         seriesPorCategoria.forEach(System.out::println);
+    }
+
+    private void filtrarSeriesPorTemporadaEAvaliacao(){
+        System.out.println("Filtrar séries até quantas temporadas? ");
+        var totalTemporadas = leitura.nextInt();
+        leitura.nextLine();
+        System.out.println("Com avaliação a partir de que valor? ");
+        var avaliacao = leitura.nextDouble();
+        leitura.nextLine();
+        List<Serie> filtroSeries = repositorio.findByTotalTemporadasLessThanEqualAndAvaliacaoGreaterThanEqual(totalTemporadas, avaliacao);
+        System.out.println("*** Séries filtradas ***");
+        filtroSeries.forEach(s ->
+                System.out.println(s.getTitulo() + " - avaliação: " + s.getAvaliacao()));
     }
 
 }
